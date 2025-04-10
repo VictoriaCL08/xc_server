3a030b6d743cb8d79c2b5629aec595be1e019c2d
VictoriaCL08 <colonlav@email.sc.edu>
2025-04-10 17:12:21 -0400

    updating pictures

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 //to start running, type nodemon server.js in terminal
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const Joi = require("joi")
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+
+
+
 app.get("/",(req, res)=>{
     res.sendFile(__dirname+"/index.html");
 });
+
+
 
 let houses =
     [
@@ -21,11 +40,11 @@ let houses =
                 "wrap around porch",
                 "attached garage"
             ],
-            "main_image": "farm.webp",
+            "main_image": "semester-events/track-pic.png",
             "floor_plans": [
                 {
                     "name": "Main Level",
-                    "image": "farm-floor1.webp"
+                    "image": "semester-events/track-pic.png"
                 },
                 {
                     "name": "Basement",
@@ -42,7 +61,7 @@ let houses =
                 "grand porch",
                 "covered deck"
             ],
-            "main_image": "mountain-house.webp",
+            "main_image": "semester-events/cookie-run",
             "floor_plans": [
                 {
                     "name": "Main Level",
@@ -94,9 +113,9 @@ let officers = [
         "name": "Carina Burdick",
         "major": "Excercise Science",
         "year": "Junior",
-        "home_town": "Irmo, SC",
+        "hometown": "Irmo, SC",
         "favorite_event": "1600m",
-        "img": "images/about-us/xc-prez.png"
+        "img": "xc-prez.png"
     },
     {
         "_id": 2,
@@ -104,9 +123,9 @@ let officers = [
         "name": "Sydney Rose",
         "major": "Excercise Science",
         "year": "Junior",
-        "home_town": "Lexington, KY",
+        "hometown": "Lexington, KY",
         "favorite_event": "400m and 4x400m",
-        "img": "images/about-us/track-prez-pic.png"
+        "img": "track-prez-pic.png"
     },
     {
         "_id": 3,
@@ -114,9 +133,9 @@ let officers = [
         "name": "Ellie Toth",
         "major": "Biology",
         "year": "Junior",
-        "home_town": "East Fishkill, NY",
+        "hometown": "East Fishkill, NY",
         "favorite_event": "800m and 1500m",
-        "img": "images/about-us/vp-pic.png"
+        "img": "vp-pic.png"
     },
     {
         "_id": 4,
@@ -124,9 +143,9 @@ let officers = [
         "name": "Victoria Colon-LaBorde",
         "major": "Chemical Engineering",
         "year": "Junior",
-        "home_town": "Aiken, SC",
+        "hometown": "Aiken, SC",
         "favorite_event": "5K and 1600 m",
-        "img": "images/about-us/treasurer-pic.png"
+        "img": "treasurer-pic.png"
     },
     {
         "_id": 5,
@@ -134,9 +153,9 @@ let officers = [
         "name": "Patrick Champion",
         "major": "Sports Management",
         "year": "Sophomore",
-        "home_town": "Greenville, SC",
+        "hometown": "Greenville, SC",
         "favorite_event": "10K",
-        "img": "images/about-us/secretary-pic.png"
+        "img": "secretary-pic.png"
     },
     {
         "_id": 6,
@@ -144,9 +163,9 @@ let officers = [
         "name": "Michael Davis",
         "major": "Exercise Science",
         "year": "Senior",
-        "home_town": "Columbia, SC",
+        "hometown": "Columbia, SC",
         "favorite_event": "Half Marathon",
-        "img": "images/about-us/safety-officer1.png"
+        "img": "safety-officer1.png"
     },
     {
         "_id": 7,
@@ -154,9 +173,9 @@ let officers = [
         "name": "Bri Davis",
         "major": "Nursing",
         "year": "Junior",
-        "home_town": "Redondo Beach, CA",
+        "hometown": "Redondo Beach, CA",
         "favorite_event": "200m",
-        "img": "images/about-us/safety-officer2.png"
+        "img": "safety-officer2.png"
     }
 
 ];
@@ -265,6 +284,99 @@ app.get("/api/schedule", (req, res)=>{
     res.send(schedule);
 })
 
+
+
+//Houses
+app.get("/api/houses", (req, res)=>{
+    res.send(houses);
+});
+
+app.post("/api/houses", upload.single("img"), (req,res)=>{
+    const result = validateHouse(req.body);
+
+
+    if(result.error){
+        console.log("I have an error");
+        res.status(400).send(result.error.deatils[0].message);
+        return;
+    }
+
+    const house = {
+        _id: houses.length,
+        name:req.body.name,
+        size:req.body.size,
+        bedrooms:req.body.bedrooms,
+        bathrooms:req.body.bathrooms,
+    };
+
+    if(req.file){
+        house.main_image = req.file.filename;
+    }
+
+    houses.push(house);
+    res.status(200).send(house);
+});
+
+const validateHouse = (house) => {
+    const schema = Joi.object({
+        _id:Joi.allow(""),
+        name:Joi.string().min(3).required(),
+        size:Joi.number().required().min(0),
+        bedrooms:Joi.number().required().min(0),
+        bathrooms:Joi.number().required().min(0),
+
+    });
+
+    return schema.validate(house);
+};
+
+
+//Officers
+app.get("/api/officers", (req, res)=>{
+    res.send(officers);
+});
+
+app.post("/api/officers", upload.single("img"), (req,res)=>{
+    const result = validateOfficer(req.body);
+
+
+    if(result.error){
+        console.log("I have an error");
+        res.status(400).send(result.error.deatils[0].message);
+        return;
+    }
+
+    const officer = {
+        _id: officers.length,
+        position:req.body.position,
+        name:req.body.name,
+        major:req.body.major,
+        year:req.body.year,
+        hometown: req.body.hometown,
+        favorite_event:req.body.favorite_event,
+    };
+
+    if(req.file){
+        officer.img =req.file.filename;
+    }
+
+    officers.push(officer);
+    res.status(200).send(officer);
+});
+
+const validateOfficer = (officer) => {
+    const schema = Joi.object({
+        _id:Joi.allow(""),
+        position:Joi.string().min(3).required(),
+        name:Joi.string().min(3).required(),
+        major:Joi.string().min(3).required(),
+        year:Joi.string().min(3).required(),
+        hometown:Joi.string().min(3).required(),
+        favorite_event:Joi.string().min(3).required(),
+    });
+
+    return schema.validate(officer);
+};
 
 
 app.listen(3001, ()=>{
